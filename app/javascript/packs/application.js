f3601761405f7fe327a9a271b7240f64f3b9943f
Patrice Gedeon <pgedeon84@gmail.com>
2019-09-10 20:18:48 -0400

    added content, CSS, Javascript, images

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,7 @@
+import 'bootstrap';
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+initUpdateNavbarOnScroll();
+
 /* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
@@ -16,3 +20,4 @@
 // const imagePath = (name) => images(name, true)
 
 console.log('Hello World from Webpacker')
+
